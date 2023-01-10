7c96f6f033e7ca570e5c77d8002c611c00e5114b
malatindez <axenance@gmail.com>
2023-01-10 21:32:12 +0200

    Update random.hpp

--- math/random.hpp
+++ math/random.hpp
@@ -23,7 +23,7 @@ namespace malatindez::math::random
                 p[i] = static_cast<typename T::type>(from[i] + dis(gen) * (to[i] - from[i]));
             return p;
         }
-        
+
         template<AnyVec T, Primitive U>
         inline vec<T::size, std::remove_const_t<typename T::type>> RandomVector(std::mt19937 &gen, U const &from, U const &to) noexcept
         {
@@ -49,7 +49,7 @@ namespace malatindez::math::random
     vec3 RandomVector3(std::mt19937 &gen, U const &from, U const &to) noexcept { return _detail::RandomVector<vec3>(gen, from, to); }
     template<Primitive U>
     vec4 RandomVector4(std::mt19937 &gen, U const &from, U const &to) noexcept { return _detail::RandomVector<vec4>(gen, from, to); }
-    
+
     template<AnyVec U>
     dvec2 RandomDVector2(std::mt19937 &gen, U const &from, U const &to) noexcept { return _detail::RandomVector<dvec2>(gen, from, to); }
     template<AnyVec U>
@@ -89,5 +89,57 @@ namespace malatindez::math::random
     template<Primitive U>
     uivec4 RandomUVector4(std::mt19937 &gen, U const &from, U const &to) noexcept { return _detail::RandomVector<uivec4>(gen, from, to); }
 
-    
+    namespace poisson_disc
+    {
+        namespace _detail
+        {
+            template<AnyVec U>
+            inline std::vector<U> PoissonDisc(std::mt19937 &gen, U const &from, U const &to, float min_distance, size_t max_attempts = 30) noexcept
+            {
+                std::vector<U> result;
+                std::uniform_real_distribution<float> dist(0.0f, 1.0f);
+                std::vector<U> active;
+                result.push_back(random::_detail::RandomVector<U>(gen, from, to));
+                active.push_back(result.back());
+                while (!active.empty())
+                {
+                    std::uniform_int_distribution<size_t> dist2(0, active.size() - 1);
+                    size_t index = dist2(gen);
+                    U point = active[index];
+                    bool found = false;
+                    for (size_t i = 0; i < max_attempts; ++i)
+                    {
+                        U new_point = point + random::_detail::RandomVector<U>(gen, -min_distance, min_distance);
+                        if (new_point.x < from.x || new_point.x > to.x || new_point.y < from.y || new_point.y > to.y)
+                            continue;
+                        bool ok = true;
+                        for (auto const &p : result)
+                        {
+                            if (distance(p, new_point) < min_distance)
+                            {
+                                ok = false;
+                                break;
+                            }
+                        }
+                        if (ok)
+                        {
+                            found = true;
+                            result.push_back(new_point);
+                            active.push_back(new_point);
+                            break;
+                        }
+                    }
+                    if (!found)
+                    {
+                        active.erase(active.begin() + index);
+                    }
+                }
+                return result;
+            }
+        } // namespace _detail
+        inline std::vector<vec2> Generate(std::mt19937 &gen, vec2 const &from, vec2 const &to, float min_distance, size_t max_attempts = 30) noexcept
+        {
+            return _detail::PoissonDisc<vec2>(gen, from, to, min_distance, max_attempts);
+        }
+    } // namespace poisson_disc
 } // namespace malatindez::math::random
